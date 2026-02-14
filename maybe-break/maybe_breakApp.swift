35e9b7db4c267921e179019451ebc62a86edc21c
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2026-02-14 00:41:38 -0500

    Initial Commit

---
+++ maybe-break/maybe_breakApp.swift
@@ -0,0 +1,17 @@
+//
+//  maybe_breakApp.swift
+//  maybe-break
+//
+//  Created by Eric Wang on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct maybe_breakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
